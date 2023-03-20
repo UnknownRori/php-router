a2d48190f08c2e9c84dd640a5cdc7899723fc3b2
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-03-21 06:33:40 +0700

    🛠️ Changes the Exceptions

--- src/Exceptions/RouteNotFoundException.php
+++ src/Exceptions/RouteNotFoundException.php
@@ -6,8 +6,12 @@ use RuntimeException;
 
 class RouteNotFoundException extends RuntimeException
 {
-    public function __construct(string $url, string $method)
+    public function __construct(string $url = '', string $method = '', string $key = null)
     {
-        $this->message = "Route with URL '{$url}' and with HTTP Method '{$method}' cannot be found!";
+        if (!is_null($key)) {
+            $this->message = "Route with URL '{$url}' and with HTTP Method '{$method}' cannot be found!";
+        } else {
+            $this->message = "Cannot find route that has name of '{$key}'";
+        }
     }
 }
