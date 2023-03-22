dbfd0e11f7f9f798af55ae33a02136c2161f9e98
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-03-23 03:48:00 +0700

    📦 Added some helper method

--- src/Routes.php
+++ src/Routes.php
@@ -258,6 +258,66 @@ class Routes implements ToArray, FromArray, Serializable
     }
 
     /**
+     * For easy typing on built in constraint
+     *
+     * @param  string|array $placeholder
+     *
+     * @return self
+     */
+    public function whereAlphaNum(string|array $placeholder): self
+    {
+        if (!is_array($placeholder)) {
+            return $this->where([$placeholder => 'alphanum']);
+        }
+
+        foreach ($placeholder as $key => $value) {
+            $this->where([$value => 'alphanum']);
+        }
+
+        return $this;
+    }
+
+    /**
+     * For easy typing on built in constraint
+     *
+     * @param  string|array $placeholder
+     *
+     * @return self
+     */
+    public function whereNumeric(string|array $placeholder): self
+    {
+        if (!is_array($placeholder)) {
+            return $this->where([$placeholder => 'numeric']);
+        }
+
+        foreach ($placeholder as $key => $value) {
+            $this->where([$value => 'numeric']);
+        }
+
+        return $this;
+    }
+
+    /**
+     * For easy typing on built in constraint
+     *
+     * @param  string|array $placeholder
+     *
+     * @return self
+     */
+    public function whereAlpha(string|array $placeholder): self
+    {
+        if (!is_array($placeholder)) {
+            return $this->where([$placeholder => 'alpha']);
+        }
+
+        foreach ($placeholder as $key => $value) {
+            $this->where([$value => 'alpha']);
+        }
+
+        return $this;
+    }
+
+    /**
      * Create URL using named Route and then passed some args for dynamically insert the data to the dynamic route
      * ## Still Experimental API
      *
